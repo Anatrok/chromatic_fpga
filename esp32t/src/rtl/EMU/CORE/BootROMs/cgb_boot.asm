85c5f2a7d53bb9372cff4e8b096e77b9617e6c50
Anatrok <Anatrok@gmail.com>
2025-01-16 00:54:04 -0700

    update make file and asm for compatibility with rgbds Comment out Platform logic due to not working in my environment

--- esp32t/src/rtl/EMU/CORE/BootROMs/cgb_boot.asm
+++ esp32t/src/rtl/EMU/CORE/BootROMs/cgb_boot.asm
@@ -1011,11 +1011,11 @@ LoadBGPalettes:
 LoadPalettes:
     ld a, $80
     or e
-    ld [c], a
+    ldh [c], a
     inc c
 .loop
     ld a, [hli]
-    ld [c], a
+    ldh [c], a
     dec d
     jr nz, .loop
     ret
